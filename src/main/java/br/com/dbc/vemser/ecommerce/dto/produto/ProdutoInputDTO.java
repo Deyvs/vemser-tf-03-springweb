fa283a42b142cd03b8e05ae8307be9180f0f0ace
ricardo <rosastaricardo84@gmail.com>
2023-08-05 17:14:14 -0300

    produto service metodos listar e buscar

---
+++ src/main/java/br/com/dbc/vemser/ecommerce/dto/produto/ProdutoInputDTO.java
@@ -0,0 +1,31 @@
+package br.com.dbc.vemser.ecommerce.dto.produto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProdutoInputDTO {
+
+    @NotNull
+    @NotBlank
+    private String modelo;
+
+    @NotNull
+    @NotBlank
+    private String tamanho;
+    @NotNull
+    @NotBlank
+    private String cor;
+    @NotNull
+    @NotBlank
+    private String setor;
+    @NotNull
+    @NotBlank
+    private Double valor;
+}
--- src/test/java/br/com/dbc/vemser/ecommerce/EcommerceApplicationTests.java
+++ src/test/java/br/com/dbc/vemser/ecommerce/EcommerceApplicationTests.java
@@ -1,7 +1,13 @@
 package br.com.dbc.vemser.ecommerce;
 
+import br.com.dbc.vemser.ecommerce.entity.Produto;
+import br.com.dbc.vemser.ecommerce.exceptions.BancoDeDadosException;
+import br.com.dbc.vemser.ecommerce.repository.ProdutoRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class EcommerceApplicationTests {
