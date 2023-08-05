de5941dde7ed3b1c4b246aa610ae777eb99982b2
milit <militaonewperfil@gmail.com>
2023-08-05 17:17:01 -0300

    feat:  repository pedido

--- src/main/java/br/com/dbc/vemser/ecommerce/entity/Pedido.java
+++ src/main/java/br/com/dbc/vemser/ecommerce/entity/Pedido.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
