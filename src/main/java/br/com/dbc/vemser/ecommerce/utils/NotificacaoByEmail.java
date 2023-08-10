b5e63df5502cdbd1011fd29007adf144cc97b826
Deyvison Silva <deyvisondasilva@gmail.com>
2023-08-10 13:17:02 -0300

    update: msg email pedido

--- src/main/java/br/com/dbc/vemser/ecommerce/utils/NotificacaoByEmail.java
+++ src/main/java/br/com/dbc/vemser/ecommerce/utils/NotificacaoByEmail.java
@@ -3,6 +3,7 @@ package br.com.dbc.vemser.ecommerce.utils;
 
 import br.com.dbc.vemser.ecommerce.dto.cliente.ClienteDTO;
 import br.com.dbc.vemser.ecommerce.dto.pedido.PedidoDTO;
+import br.com.dbc.vemser.ecommerce.entity.Produto;
 import br.com.dbc.vemser.ecommerce.service.EmailService;
 import lombok.Data;
 import org.springframework.stereotype.Component;
@@ -46,8 +47,22 @@ public class NotificacaoByEmail {
     public void notificarByEmailPedidoCliente(ClienteDTO clienteDTO, PedidoDTO pedidoDTO) throws MessagingException {
         HashMap<String, String> dados = new HashMap<>();
         dados.put("nome", clienteDTO.getNome());
+        String pedidos = "";
+        for(int i = 0; i < pedidoDTO.getProdutos().size(); i++) {
+            String concat = "<br>" + pedidoDTO.getProdutos().get(i).getModelo() +
+                    " - " + pedidoDTO.getProdutos().get(i).getTamanho() +
+                    ": R$ " +
+                    String.format("%.2f", pedidoDTO.getProdutos().get(i).getValor());
+
+            pedidos += concat;
+        }
+
         String response = "Seu pedido foi <b>finalizado</b> com sucesso" +
-                "<br>Valor Total do Pedido: R$ " + String.format("%.2f", pedidoDTO.getValor());
+                "<br>--------------------------------------------------" +
+                pedidos +
+                "<br>--------------------------------------------------" +
+                "<br>Valor Total do Pedido | R$ " + String.format("%.2f", pedidoDTO.getValor()) +
+                "<br>--------------------------------------------------";
         dados.put("status", response);
 
         emailService.sendTemplateEmail(dados, clienteDTO.getEmail());
