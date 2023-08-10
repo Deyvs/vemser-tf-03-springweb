ce32f934594fb02bbe7e6461b5b06d2bdce1568e
milit <militaonewperfil@gmail.com>
2023-08-09 22:31:07 -0300

    fix:enderco

--- src/main/java/br/com/dbc/vemser/ecommerce/entity/Endereco.java
+++ src/main/java/br/com/dbc/vemser/ecommerce/entity/Endereco.java
@@ -4,10 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -22,19 +18,6 @@ public class Endereco {
     private String bairro;
     private String cidade;
     private String estado;
-    @NotNull
-    private String tipoEndereco;
-
-    @NotNull
-    private String bairro;
-
-
-    @NotNull
-    private String pais;
-
-
-
-
 }
 
 
